e3b48418ce0208a87df836b885c163096ca05e1b
s-shemmee <shemmee.dev@gmail.com>
2024-10-22 22:54:15 +0100

    feat: add social media and action icons

---
+++ src/components/icons/DribbbleIcon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const DribbbleIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-dribbble"
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <path d="M8.56 2.75c4.37 6.03 6.02 9.42 8.03 17.72"></path>
+    <path d="M19.13 4.09c-3.72 4.35-8.94 5.66-16.88 5.85"></path>
+    <path d="M20.75 11.84c-3.5-.93-6.63-.82-8.94 0-2.58.92-5.01 2.86-7.44 6.32"></path>
+  </svg>
+);
+
+export default DribbbleIcon;
---
+++ src/components/icons/PolygonIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PolygonIcon = () => (
+  <svg
+    fill="#E0D0F2"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    enableBackground="new 0 0 24 24"
+  >
+    <path d="M21.9,11.5l-4.5-7.8c-0.2-0.3-0.5-0.5-0.9-0.5h-9c-0.4,0-0.7,0.2-0.9,0.5l-4.5,7.8c-0.2,0.3-0.2,0.7,0,1l4.5,7.8c0.2,0.3,0.5,0.5,0.9,0.5h9c0.4,0,0.7-0.2,0.9-0.5l4.5-7.8C22,12.2,22,11.8,21.9,11.5z" />
+  </svg>
+);
+
+export default PolygonIcon;
